c603d0c09980ee636f1187f9e79dd8759a29f6eb
VHarisop <vharisop@gmail.com>
2018-08-26 23:32:17 -0400

    Some minor code

--- code_examples/sketch.jl
+++ code_examples/sketch.jl
@@ -2,6 +2,10 @@
 
 @enum sketchType subg=1 ros=2
 
+# low rank matrix generation
+lowrank(n, d, rank) = randn(n, rank) * randn(rank, d)
+
+
 """
 	sketchmat_subg(width::Number, delta::Float64, n::Int)
 
@@ -30,5 +34,25 @@ Sketch and solve a least squares problem aiming for a ``\\delta`` approximation
 under an appropriate sketch type.
 """
 function sketchls(A, x, stype::sketchType, delta)
-	throw(ErrorException("Unimplemented operation!"))
+	if stype == subg
+		return sketchls_subg(A, x, delta)
+	elseif stype == ros
+		throw(ErrorException("Unimplemented operation!"))
+	end
+end
+
+
+"""
+	sketchls_subg(A, x, delta)
+
+Sketches a least squares problem without constraints using a subgaussian matrix
+and solves the problem analytically. Returns the ratio
+``\\frac{\\hat{x}}{x^{\\star}}``, where ``\\hat{x}`` is the solution to the
+sketch problem and ``x^{\\star}`` is the solution to the original problem.
+"""
+function sketchls_subg(A, x, delta)
+	n, d = size(A); r = rank(A); S = sketchmat_subg(sqrt(r), delta, n)
+	Q = S * A; y = A * x; b = S * y
+	xopt = A \ y; xhat = Q \ b
+	return xhat / xopt
 end
